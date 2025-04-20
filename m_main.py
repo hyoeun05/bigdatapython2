37ca9ca546a5d391c3459a1d793a7b0afe0e558d
hyoeun05 <khe4757628@gmail.com>
2025-04-20 10:50:46 +0000

    함수 사용 업데이트 완료

--- m_main.py
+++ m_main.py
@@ -1,8 +1,8 @@
 import func  
 
-func.m100()      # TOP 100곡 출력
-func.m50()       # TOP 50곡 출력
-func.m10()       # TOP 10곡 출력
-func.m_random()  # 랜덤 추천
-func.m_search()  # 가수 검색
-func.m_file()    # 파일 저장
+func.m100()      # python -c "import func; func.m100()"
+func.m50()       # python -c "import func; func.m50()"
+func.m10()       # python -c "import func; func.m_random()"
+func.m_random()  # python -c "import func; func.m_random()"
+func.m_search()  # python -c "import func; func.m_search()"
+func.m_file()    # python -c "import func; func.m_file()"
