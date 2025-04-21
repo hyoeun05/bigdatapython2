70fdead3e393da4dcc6e550493127e0b5c6def71
hyoeun05 <khe4757628@gmail.com>
2025-04-21 06:10:52 +0000

    함수 사용

--- m_main.py
+++ m_main.py
@@ -2,7 +2,7 @@ import func
 
 func.m100()      # python -c "import func; func.m100()"
 func.m50()       # python -c "import func; func.m50()"
-func.m10()       # python -c "import func; func.m_random()"
+func.m10()       # python -c "import func; func.m10()"
 func.m_random()  # python -c "import func; func.m_random()"
 func.m_search()  # python -c "import func; func.m_search()"
 func.m_file()    # python -c "import func; func.m_file()"
